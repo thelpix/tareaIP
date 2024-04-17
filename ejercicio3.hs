026c319f69996b7846ee5782b7a139f8a249b4cf
thelpix <ivan123456274@gmail.com>
2024-04-17 19:23:53 -0300

    borrando archivos

--- ejercicio3.hs
+++
@@ -1,6 +0,0 @@
-estanRelacionados :: Integer -> Integer -> Bool
-estanRelacionados a b | a /= 0 && rem a b == 0 && a*a+b*a*(-a) `div` b == 0 = True
-                      | otherwise = False
-
-
---- PODES USAR MULTIPLO PORQUE SI FACTORIZAS, SALE QUE ESTA RELACIONADO CON A, A RELACIONADO CON B, SI ES MULTIPLO DE A
