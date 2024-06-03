45687778f00d06a5dd2daadc2cba133da0e9260e
thelpix <ivan123456274@gmail.com>
2024-06-02 21:49:32 -0300

    organizando cositas

--- Guia3/ejercicioSumaD.hs
+++
@@ -1,6 +0,0 @@
-sumaDist :: Int -> Int -> Int -> Int
-sumaDist x y z | x == y && x/= z = x+z
-               | y == z && y/= x = y+x
-               | z == x && z/= y = y+z
-               | x == y && x == z = 0
-               | otherwise = x + y + z
